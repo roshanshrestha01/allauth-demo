ef607afd321d67589eb7dc9a88a49d667dc645cd
Roshan Shrestha <roshanshrestha01@gmail.com>
2019-06-22 10:15:13 +0545

    Enhanced user admin
    User admin list to be able to search with full name, email and username.

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import User
 
-admin.site.register(User)
+
+class UserCustomAdmin(admin.ModelAdmin):
+    list_display = ['username', 'full_name', 'email']
+    search_fields = ['username', 'full_name', 'email']
+
+
+admin.site.register(User, UserCustomAdmin)
