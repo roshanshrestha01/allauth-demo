a0e1bba7693e67a84c70e134336eb7828b016269
Roshan Shrestha <roshanshrestha01@gmail.com>
2019-06-22 10:09:38 +0545

    Template and static directory location settings added

--- social_login/settings/base.py
+++ social_login/settings/base.py
@@ -28,7 +28,9 @@ ROOT_URLCONF = 'social_login.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -39,6 +41,10 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
 ]
 
 WSGI_APPLICATION = 'social_login.wsgi.application'
