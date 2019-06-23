45061ff96d43b769ac42a1973740a0af328d4e77
Roshan Shrestha <roshanshrestha01@gmail.com>
2019-06-23 09:49:38 +0545

    Post git hook to run collectstatic

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@
 cd ../app/
 pip install -r requirements/prod.txt  | grep -v 'Requirement already satisfied' | grep -v 'Cleaning up...'
 ./manage.py migrate -v 0
+./manage.py collectstatic
 circusctl restart fbapp
