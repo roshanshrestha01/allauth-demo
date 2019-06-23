91f5cba85fe72c49498861bfe53336fc82e36388
Roshan Shrestha <roshanshrestha01@gmail.com>
2019-06-23 22:19:20 +0545

    Privacy policy page added

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.HomeTemplateView.as_view(), name='home'),
     path('sign-up/', views.SignUpTemplateView.as_view(), name='sign-up'),
     path('deactivate/', views.DeactivateView.as_view(), name='deactivate'),
+    path('privacy-policy/', views.TemplateView.as_view(template_name='users/privacy-policy.html'), name='privacy-policy'),
 ]
