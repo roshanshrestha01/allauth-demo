426a9c94ad8c0f5cd9015ac6e5595af2ea941f6e
Roshan Shrestha <roshanshrestha01@gmail.com>
2019-06-23 08:53:33 +0545

    Separate email backend for development and production

--- social_login/settings/base.py
+++ social_login/settings/base.py
@@ -81,12 +81,3 @@ AUTHENTICATION_BACKENDS = (
 )
 
 SITE_ID = 1
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'roshanshrestha01@gmail.com'
-DEFAULT_FROM_EMAIL = 'roshanshrestha01@gmail.com'
-SERVER_EMAIL = 'roshanshrestha01@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ['GMAIL_PASSWORD']
--- social_login/settings/production.py
+++ social_login/settings/production.py
@@ -29,3 +29,13 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'roshanshrestha01@gmail.com'
+DEFAULT_FROM_EMAIL = 'roshanshrestha01@gmail.com'
+SERVER_EMAIL = 'roshanshrestha01@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ['GMAIL_PASSWORD']
